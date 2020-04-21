bae125817bf05c361eedb95ffce75927c75e2be1
Wright <wrightkhlebisol@gmail.com>
2020-04-21 03:32:40 +0100

    Created assignments resource

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,11 +10,7 @@ class PostsController extends Controller
     //
     public function show($slug)
     {
-        $post = Post::where('slug', $slug)->first();
-
-        if (!$post) {
-            abort('404');
-        }
+        $post = Post::where('slug', $slug)->firstOrFail();
 
         return view('test', ['name' => $post]);
     }
