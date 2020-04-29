befcd36c45240b209b2455a6dd2cfc25c042b71a
Wright <wrightkhlebisol@gmail.com>
2020-04-29 19:48:57 +0100

    Created contact email view

---
+++ resources/views/articles/contact.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('style')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.2/css/bulma.min.css">
+    <style>
+        form.bg-white.p-6.rounded.shadow-md {
+            margin: 100px auto;
+            background-color: white;
+            padding: 20px;
+        }
+
+        input#email{
+            width: 100%;
+            padding: 5px;
+        }
+
+        button.bg-blue-500 {
+            width: 100%;
+            border: none;
+            border-radius: 7px;
+            padding: 8px;
+            color: white;
+            background-color: #3298dc;
+        }
+
+        div.text-red-500.text-xs{
+            color: red;
+        }
+
+    </style>
+@endsection
+
+@section('content')
+<form action="/contact" method="POST" class="bg-white p-6 rounded shadow-md" style="width: 300px">
+    @csrf
+    <div class="mb-5 field">
+        <label for="email" class="label black text-xs uppercase font-semibold mb-1">
+            EMAIL ADDRESS
+        </label>
+        <input type="email" required placeholder="you@website.com" id="email" name="email" class="border px-2 py-1 text-sm m-full">
+    </div>
+    @error('email')
+        <div class="text-red-500 text-xs">{{$message}}</div>
+    @enderror
+
+    <button type="submit" class="bg-blue-500">
+        Email Me
+    </button>
+
+</form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/articles', 'ArticlesController@index')->name('articles.index');
 
+Route::get('/contact', 'ContactController@index');
+
+Route::post('/contact', 'ContactController@store');
+
 Route::get('/articles/create', 'ArticlesController@create')->name('articles.create');
 
 Route::get('/articles/{article}', 'ArticlesController@show')->name('articles.show');
